cec5f81d2750fc2d8c4606d3fd0217674f6ec1cc
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-05-04 23:49:51 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,12 +5,14 @@ const button = document.getElementById("b");
 button.addEventListener("click", () => {
   let LENGTH = 0;
   let _LOOP = 0;
+  let _EQUAL = 1;
   const msg = ["Please enter a input"];
   if (!firstInput.value || !secondInput.value) {
     while (_LOOP <= firstInput.value) {
       for (let _i = _LOOP; _i <= firstInput.value; _i++) {
         if (_LOOP == firstInput.value) {
-          console.log(msg[LENGTH]);
+          let _k = _LOOP + _EQUAL;
+          console.log(msg[(LENGTH = _k - _EQUAL)]);
         }
       }
       _LOOP++;
