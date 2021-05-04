9eb4f13e6c15b0f815c2f8f500e84fe49a07c982
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-05-04 23:48:00 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-{}
+const firstInput = document.getElementById("f");
+const secondInput = document.getElementById("s");
+const button = document.getElementById("b");
+
+button.addEventListener("click", () => {
+  let LENGTH = 0;
+  let _LOOP = 0;
+  const msg = ["Please enter a input"];
+  if (!firstInput.value || !secondInput.value) {
+    while (_LOOP <= firstInput.value) {
+      for (let _i = _LOOP; _i <= firstInput.value; _i++) {
+        if (_LOOP == firstInput.value) {
+          console.log(msg[LENGTH]);
+        }
+      }
+      _LOOP++;
+    }
+  }
+});
