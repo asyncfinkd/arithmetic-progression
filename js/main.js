1c2bc9683330fa959573696396f1421588b37434
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-05-05 00:15:36 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-const firstInput = document.getElementById("f");
-const secondInput = document.getElementById("s");
-const button = document.getElementById("b");
+let firstInput = document.getElementById("f");
+let secondInput = document.getElementById("s");
+let button = document.getElementById("b");
+let paragraph = document.getElementById("demo");
 
 button.addEventListener("click", () => {
   let LENGTH = 0;
   let _LOOP = 0;
   let _EQUAL = 1;
-  const msg = ["Please enter a input"];
+  const msg = ["Please enter a input", "system error", "this is dummie number"];
   let structure = [firstInput.value, secondInput.value];
-  if (!structure[LENGTH] && !structure[1]) {
+  if (!structure[LENGTH] && !structure[_EQUAL]) {
     structure.map((i) => {
       if (i != "") {
         console.log("structure is working");
@@ -18,10 +19,27 @@ button.addEventListener("click", () => {
       for (let _i = _LOOP; _i <= structure[LENGTH]; _i++) {
         if (_LOOP == structure[LENGTH]) {
           let _k = _LOOP + _EQUAL;
-          console.log(msg[(LENGTH = _k - _EQUAL)]);
+          alert(msg[(LENGTH = _k - _EQUAL)]);
         }
       }
       _LOOP++;
+    }
+  } else {
+    if (!structure[_EQUAL]) {
+      alert(msg[LENGTH]);
+    } else {
+      if (_LOOP != LENGTH && LENGTH != LENGTH) {
+        console.log(msg[_EQUAL]);
+      } else {
+        if (structure[LENGTH] <= LENGTH || structure[_EQUAL] <= LENGTH) {
+          if (msg[2]) alert(msg[2]);
+        } else {
+          let _LENGTH_INPUT = 2;
+          let _l = (+structure[LENGTH] + +structure[_EQUAL]) / _LENGTH_INPUT;
+          paragraph.innerHTML = _l;
+          console.log(_l);
+        }
+      }
     }
   }
 });
